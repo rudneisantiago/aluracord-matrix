e0e1ce0b2fd3517e8c851050421a4bafe3f37c8b
Rudnei Santiago <rudneisantiago@santos.sp.gov.br>
2022-01-28 14:06:36 -0300

    Solução do desafio aula 2 a grosso modo. Testar _.debounce para substituição.

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,7 @@
+import React from "react";
+import { useRouter } from "next/router";
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
 import appConfig from "../config.json";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
 
 function Title(props) {
   const Tag = props.tag;
@@ -49,22 +20,13 @@ function Title(props) {
   );
 }
 
-// function HomePage() {
-//     return (
-//         <div>
-//             <GlobalStyle />
-//             <Title titulo='Boas Vindas de Volta!' tag="h1"></Title>
-//             <h2>Discord - Alura Matrix</h2>
-//         </div>
-//     )
-// }
-
 export default function PaginaInicial() {
-  const username = "peas";
+  const [username, setUsername] = React.useState("");
+  const [searchField, setSearchField] = React.useState("");
+  const router = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -99,6 +61,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(event) => {
+              event.preventDefault();
+              router.push("/chat");
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -129,6 +95,15 @@ export default function PaginaInicial() {
                   mainColorHighlight: appConfig.theme.colors.primary[500],
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
+              }}
+              value={searchField}
+              onChange={(event) => {
+                const { value } = event.target;
+                setSearchField(value);
+
+                if (searchField.length > 2) {
+                  setUsername(value);
+                }
               }}
             />
             <Button
